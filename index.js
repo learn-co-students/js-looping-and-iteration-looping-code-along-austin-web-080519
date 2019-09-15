16b39a3dd485cb0a21553ee01f675f32606b4b83
andrewrosario <rosario.andrew@gmail.com>
2019-09-15 13:16:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let allMessages = []
+    for (let i = 0; i<array.length; i++) {
+        let message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        allMessages.push(message)
+    }
+    return allMessages
+}       
+
+function countdown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--
+    }
+}
