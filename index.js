8074a6961a8ed394619c8e9362eb177f3073b799
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-15 13:29:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+cards = []
+
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        note = (`Thank you, ${array[i]}, for this wonderful ${event} gift!`)
+        console.log(note)
+        cards.push(note)
+    }
+    return cards
+}
