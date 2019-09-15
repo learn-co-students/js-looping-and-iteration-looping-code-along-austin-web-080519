363451b54228a0ff359b0ae0c08b23f6cfa9db46
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-15 13:35:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Code your solutions in this file
 
-cards = []
+let cards = []
 
 
 function writeCards(array, event) {
     for (let i = 0; i < array.length; i++) {
-        note = (`Thank you, ${array[i]}, for this wonderful ${event} gift!`)
+        let note = (`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
         console.log(note)
         cards.push(note)
     }
     return cards
+}
+
+function countdown(number) {
+    let countdown = 0;
+    while (countdown <= number) {
+        console.log(number)
+        number--
+    }
 }
